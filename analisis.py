511a97f96046f900ee7660ba1699fde3d808539f
carlos enrique <024000123@upsrj.edu.mx>
2026-02-28 16:54:30 -0600

    Update analisis.py

--- analisis.py
+++ analisis.py
@@ -2,6 +2,6 @@
 # Fecha de modificación: 28 de febrero de 2026
 # Comentario: Script inicial para la lectura y exploración del dataset del equipo.
 
-print("--- Iniciando el sistema de análisis de datos ---")
+print("--- Comesando análisis  ---")#modifique el print -carlos enrique 28/02/2026
 print("Buscando el archivo 'datos.csv' generado por el Colaborador A...")
 print("Carga de datos simulada y análisis preliminar completado con éxito.")

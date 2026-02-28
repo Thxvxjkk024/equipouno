2f6fe7bab81c27c929ca7e20ec15a0d21ebbc1e2
CarlosEG123 <024000123@upsrj.edu.mx>
2026-02-28 14:32:31 -0600

    Update analisis.py
    Modifique la linea 5

--- analisis.py
+++ analisis.py
@@ -2,7 +2,7 @@
 # Fecha de modificación: 28 de febrero de 2026
 # Comentario: Script inicial para la lectura y exploración del dataset del equipo.
 
-print("--- Iniciando el sistema de análisis de datos ---")
+print("--- Comenzando el analizis de datos ---")  # Cambie a Comenzando Autor:Carlos enrique guerra rivera 28/02/2026
 print("Buscando el archivo 'datos.csv' generado por el Colaborador A...")
 print("Carga de datos simulada y análisis preliminar completado con éxito.")
 

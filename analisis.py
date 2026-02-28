ee3eb72511bd4afe28cc68ab80a47f44554ff64e
CarlosEG123 <024000123@upsrj.edu.mx>
2026-02-28 14:05:04 -0600

    Modificacion del archivo analisis.py -carlos-28/02/2026
    Agrege mensaje adicional indicando revision del colaborador A

--- analisis.py
+++ analisis.py
@@ -7,3 +7,8 @@ print("Buscando el archivo 'datos.csv' generado por el Colaborador A...")
 print("Carga de datos simulada y análisis preliminar completado con éxito.")
 
 # Nota para el futuro: Aquí usaremos librerías como Pandas para limpiar los datos reales.
+
+# Modificado:carlos enrique guerra rivera
+# Fecha: 28/02/2026
+
+print("Revision realizada por colaborador A.")
